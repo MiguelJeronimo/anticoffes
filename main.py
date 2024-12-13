8311f24b4d40497da8dd5513c73866e25f8e64fa
Miguel Jerónimo <mjeronimo@frd.org.mx>
2024-12-13 13:21:09 -0600

    Implementacion del flujo de detectar patrones, bloqueo de pantalla y tomar fotografia

--- main.py
+++ main.py
@@ -1,17 +1,62 @@
-# This is a sample Python script.
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from camara.Camara import Camara
 import cv2
+from pynput import keyboard
+from pynput.keyboard import KeyCode
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from camara.Camara import Camara
+from utils.Data import Data
+from utils.Windows import Windows
+from utils.matches import matches
+
+data = Data()
+windows = Windows()
+def on_press(key:KeyCode):
+    try:
+        print(f"{str(key)}")
+        if str(key) == "Key.space":
+            data.wire_txt("dictionary/frases.txt", 'a', " ")
+        data.wire_txt("dictionary/frases.txt", 'a', key.char)
+    except AttributeError:
+        print('special key {0} pressed'.format(key))
 
 
-# Press the green button in the gutter to run the script.
+def on_release(key):
+    #detener el script al precionar esc
+    if key == keyboard.Key.enter:
+        text = data.read_txt('dictionary/frases.txt')
+        json = data.read_json('dictionary/dictionary.json')
+        match = matches(text, json['dictionaty'])
+        if match:
+            print(f"si se encontro {text} en el diccionario")
+            windows.blocked()
+        # Stop listener
+        return False
+    if key == keyboard.Key.space:
+        text = data.read_txt('dictionary/frases.txt')
+        json = data.read_json('dictionary/dictionary.json')
+        match = matches(text, json['dictionaty'])
+        if match:
+            print(f"si se encontro {text} en el diccionario")
+            windows.blocked()
+            cam = cv2.VideoCapture(0)
+            camera = Camara(camera=cam, cv2=cv2)
+            camera.capture("coffes")
+            camera.close()
+
+    if key == keyboard.Key.esc:
+        data.wire_txt("dictionary/frases.txt", 'w', "")
+        #windows.blocked()
+        # Stop listener
+        return False
+
 if __name__ == '__main__':
-    cam = cv2.VideoCapture(0)
-    camera = Camara(camera=cam, cv2= cv2)
-    camera.capture("coffes")
-    camera.close()
+    # Collect events until released
+    with keyboard.Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()
+
+    # ...or, in a non-blocking fashion:
+    listener = keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release)
+    listener.start()
